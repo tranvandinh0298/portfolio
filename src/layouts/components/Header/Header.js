b0763b295db0602fbe9a8a9568cc21f9fdd7cef3
dinhtv <64520320+TranVanDinh1998@users.noreply.github.com>
2023-05-05 19:44:22 +0700

    Update Header.js

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
-import { Collapse, Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Button from '~/components/Button';
@@ -22,97 +22,6 @@ function Header() {
         };
         window.addEventListener('scroll', handleScroll);
     }, []);
-
-    // return (
-    //     <header
-    //         className={cx('wrapper', {
-    //             sticky,
-    //         })}
-    //     >
-    //         <Container className={cx('inner')}>
-    //             <Link to={config.routes.home} className={cx('logo')}>
-    //                 <img src={images.logo} alt="Sb" />
-    //             </Link>
-    //             <button
-    //                 className={cx('nav__toggle', { show: showNav })}
-    //                 onClick={handleMenuToggle}
-    //                 aria-controls="navbar"
-    //                 aria-expanded={showNav}
-    //             >
-    //                 <span></span>
-    //                 <span></span>
-    //                 <span></span>
-    //             </button>
-    //             <Collapse in={showNav}>
-    //                 <nav id="navbar" className={cx('nav')}>
-    //                     <ul className={cx('nav__list')}>
-    //                         <li className={cx('nav__item')}>
-    //                             <Button
-    //                                 to={config.routes.home}
-    //                                 underline
-    //                                 leftIcon={<FontAwesomeIcon icon={faHouse} />}
-    //                                 onClick={() => setShowNav(false)}
-    //                             >
-    //                                 Home
-    //                             </Button>
-    //                         </li>
-    //                         <li className={cx('nav__item')}>
-    //                             <Button
-    //                                 to={config.routes.about}
-    //                                 underline
-    //                                 leftIcon={<FontAwesomeIcon icon={faUser} />}
-    //                                 onClick={() => setShowNav(false)}
-    //                             >
-    //                                 About
-    //                             </Button>
-    //                         </li>
-    //                         <li className={cx('nav__item')}>
-    //                             <Button
-    //                                 to={config.routes.projects}
-    //                                 underline
-    //                                 leftIcon={<FontAwesomeIcon icon={faSquarePollHorizontal} />}
-    //                                 onClick={() => setShowNav(false)}
-    //                             >
-    //                                 Projects
-    //                             </Button>
-    //                         </li>
-    //                         <li className={cx('nav__item')}>
-    //                             <Button
-    //                                 to={config.routes.resume}
-    //                                 underline
-    //                                 leftIcon={<FontAwesomeIcon icon={faFileLines} />}
-    //                                 onClick={() => setShowNav(false)}
-    //                             >
-    //                                 Resume
-    //                             </Button>
-    //                         </li>
-    //                         <li className={cx('nav__item')}>
-    //                             <Button
-    //                                 to={config.routes.blogs}
-    //                                 underline
-    //                                 leftIcon={<FontAwesomeIcon icon={faPenNib} />}
-    //                                 onClick={() => setShowNav(false)}
-    //                             >
-    //                                 Blogs
-    //                             </Button>
-    //                         </li>
-    //                         <li className={cx('nav__item')}>
-    //                             <Button
-    //                                 href="https://github.com/tranvandinh0298"
-    //                                 primary
-    //                                 effect="jump"
-    //                                 leftIcon={<FontAwesomeIcon icon={faGithub} />}
-    //                                 onClick={() => setShowNav(false)}
-    //                             >
-    //                                 GitHub
-    //                             </Button>
-    //                         </li>
-    //                     </ul>
-    //                 </nav>
-    //             </Collapse>
-    //         </Container>
-    //     </header>
-    // );
 
     return (
         <Navbar expanded={showNav} fixed="top" expand="md" className={cx('wrapper', { sticky })}>
